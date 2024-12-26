38c19e318ef609ae3e12e61acfb8fcb603b98470
bistaprapannabista <sushant07.dhimal@gmail.com>
2024-12-26 20:33:22 +0545

    build fix

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -30,6 +30,7 @@ export function Testimonials() {
       designation: "Engineering Lead at DataPro",
       src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
+
     {
       quote:
         "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
