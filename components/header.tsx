f2dc1af8cf619434357516fb0f1d9be082974ae7
Prapanna Bista <sushant07.dhimal@gmail.com>
2025-08-31 20:41:10 +0545

    user issue fix

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,6 @@ export default function Header() {
 
   return (
     <>
-      {/* Overlay */}
       {sidebarOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-40"
